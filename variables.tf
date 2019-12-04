196c6d1b5d000b884d5e1ce6a7abc4de658c4aa1
Ryan Cassidy <ryancassidy0@gmail.com>
2019-12-04 15:04:38 -0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,14 +1,6 @@
-variable "aws_access_key" {
-    description = "AWS Access Key"
-}
-
-variable "aws_secret_key" {
-    description = "AWS Secret Key"
-}
-
 variable "aws_region" {
     description = "AWS Region"
-    default = "us-east-1"
+    default = "us-west-1"
 }
 
 variable "aws_kms_key_id" {
